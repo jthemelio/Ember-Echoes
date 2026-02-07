2eea77d6d3c357b6d469aa1fb7f8a6f551761de9
jthemelio <jordan@themelio.co>
2026-02-08 02:08:17 +1000

    Revert "Zenva GameDevAssistant Added"
    This reverts commit ebfbc516b1a95d97e15dfcbdfd479735fe42819e.

--- addons/gamedev_assistant/scripts/chat/reasoning_toggle.gd
+++
@@ -1,6 +0,0 @@
-                                                            
-@tool      
-extends CheckButton
-
-func _ready():
-    tooltip_text = "A more powerful LLM for more complex problems. It might take minutes to get a response. This is a PREMIUM feature."
