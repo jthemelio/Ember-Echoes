539fe4098922cd9d540b75a753b2b16975aa1925
jthemelio <jordan@themelio.co>
2026-02-11 15:49:48 +1000

    Mobile Keyboard Fixes/App Icon update

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770787511|12631975506';
+const CACHE_VERSION = '1770788654|13774744305';
 /** @type {string} */
 const CACHE_PREFIX = 'Ember Echoes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
