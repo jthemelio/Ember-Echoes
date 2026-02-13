e88c3a278f48c4e02241d396af5852dd199bab87
jthemelio <jordan@themelio.co>
2026-02-13 14:14:23 +0000

    Deploying to gh-pages from @ jthemelio/Ember-Echoes@698f5110f9e077dc5d1cacf0e7a3c0611e80e684 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770991533|4820861';
+const CACHE_VERSION = '1770992058|4785818';
 /** @type {string} */
 const CACHE_PREFIX = 'Ember Echoes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
