831d7e3ec360cccc49b41f5e920605017e14c61d
jthemelio <jordan@themelio.co>
2026-02-14 20:33:42 +1000

    Build Web Export

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771061038|56809988';
+const CACHE_VERSION = '1771065212|18941692';
 /** @type {string} */
 const CACHE_PREFIX = 'Ember Echoes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
